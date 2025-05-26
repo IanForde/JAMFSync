c51e9cdb044d2f56015d12a3bcefb39f15605f78
IanForde <ianforde@asana.com>
2025-05-26 11:50:02 +0100

    Add files via upload
    Script to run Recon, Policy and Renew JAMF commands

---
+++ JAMF_Profile_Renew.sh
@@ -0,0 +1,34 @@
+choices(){
+
+    if [[ "$USER_CHOICE" == "Jamf Recon" ]]; then
+        echo "Running Jamf Recon"
+        sleep 2
+        sudo jamf recon
+        echo "Jamf Recon Complete"
+        sleep 2
+        continue
+    elif [[ "$USER_CHOICE" == "Jamf Policy" ]]; then
+        echo "Running Jamf Policy"
+        sleep 2
+        sudo jamf policy
+        echo "Jamf Policy Complete" 
+        sleep 2
+        continue
+    elif [[ "$USER_CHOICE" == "Renew Profile" ]]; then
+        echo "Running Profile Renewal" 
+        sleep 2
+        sudo profiles -N
+        echo "Profile Renewal Complete"
+        sleep 2
+        continue
+    else    
+        echo "invalid option selected" 
+    fi
+}
+USER_CHOICE=$(osascript <<EOD
+set options to {"Jamf Recon", "Jamf Policy", "Renew Profile", "Exit"}
+choose from list options with title "JAMF Commands" with prompt "Select a JAMF command to execute:" default items {"Jamf Recon"}
+EOD
+)
+
+choices
